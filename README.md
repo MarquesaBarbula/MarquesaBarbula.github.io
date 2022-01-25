434daeb14765e23682c7a8d87db157401fcec91e
Mariana Barnola <marianabarnola@gmail.com>
2022-01-25 14:18:36 +0100

    colors custom properties and dark mode enabled

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Portfolio Project
 
-This is the source code for my portfolio website project in the Front-End engineer path at CodeCademy.
+This is the source code for my portfolio Project in the Front-End engineer path at CodeCademy.
+
+Dark mode added with the help of CSS-TRICKS (https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/#aa-using-separate-stylesheets)
+
+
 
 ## Built with
 
 * HTML
 * CSS
 * [Font Awesome](https://fontawesome.com/)
-] JavaScriptq
+] JavaScript
 
 ## Practice only
 
---
+++ script.js
@@ -0,0 +1,26 @@
+// SCRIPT TIME COUNTER
+
+        const timeContainer = document.getElementById("time-container");
+        const BIRTH_DAY = "02-23-2021";
+        const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+        const intlNumberFormatter = new Intl.NumberFormat("en-US");
+    
+        setInterval(() => {
+          const now = new Date();
+          const difference = Math.floor(
+            (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+          );
+    
+          timeContainer.innerText = intlNumberFormatter.format(difference);
+        }, 1000);
+
+
+// SCRIPT DARK MODE
+// Select the button
+        const btn = document.querySelector(".btn-toggle");
+
+        // Listen for a click on the button
+        btn.addEventListener("click", function() {
+        // Then toggle (add/remove) the .dark-theme class to the body
+        document.body.classList.toggle("dark-theme");
+        });
