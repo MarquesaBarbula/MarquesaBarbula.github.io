9cea44b97408ac90f6407e14dea14e752e69fbc4
Mariana Barnola <marianabarnola@gmail.com>
2022-01-27 21:16:51 +0100

    restore script from main after messing with it trying to fix the NAN of the countUp in Firefox

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // SCRIPT TIME COUNTER
 
-        const timeContainer = document.getElementById("time-container");
-        const BIRTH_DAY = "02-23-2021";
-        const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
-        const intlNumberFormatter = new Intl.NumberFormat("en-US");
-    
-        setInterval(() => {
-          const now = new Date();
-          const difference = Math.floor(
-            (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
-          );
-    
-          timeContainer.innerText = intlNumberFormatter.format(difference);
-        }, 1000);
+const timeContainer = document.getElementById("time-container");
+const BIRTH_DAY = "02-23-2021";
+const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+const intlNumberFormatter = new Intl.NumberFormat("en-US");
+
+setInterval(() => {
+  const now = new Date();
+  const difference = Math.floor(
+    (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+  );
+
+  timeContainer.innerText = intlNumberFormatter.format(difference);
+}, 1000);
 
 
 // SCRIPT DARK MODE
@@ -21,6 +21,6 @@ const btn = document.querySelector("#checkbox");
 
 // Listen for a click on the button
 btn.addEventListener("click", function() {
-  // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle("dark-theme");
+// Then toggle (add/remove) the .dark-theme class to the body
+document.body.classList.toggle("dark-theme");
 });
