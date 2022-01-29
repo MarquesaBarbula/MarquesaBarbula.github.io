d010df13f7e2f5ae952d9bcaf362e18a46821822
Mariana Barnola <marianabarnola@gmail.com>
2022-01-29 13:38:58 +0100

    countUp commented to hide in html and js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 // SCRIPT TIME COUNTER
 
-const timeContainer = document.getElementById("time-container");
-const BIRTH_DAY = "02-23-2021";
-const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
-const intlNumberFormatter = new Intl.NumberFormat("en-US");
+// const timeContainer = document.getElementById("time-container");
+// const BIRTH_DAY = "02-23-2021";
+// const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+// const intlNumberFormatter = new Intl.NumberFormat("en-US");
 
-setInterval(() => {
-  const now = new Date();
-  const difference = Math.floor(
-    (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
-  );
+// setInterval(() => {
+//  const now = new Date();
+//  const difference = Math.floor(
+//    (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+//  );
 
-  timeContainer.innerText = intlNumberFormatter.format(difference);
-}, 1000);
+//  timeContainer.innerText = intlNumberFormatter.format(difference);
+// }, 1000);
 
 
 // SCRIPT DARK MODE
+
 // Select the button
 const btn = document.querySelector("#checkbox");
 
