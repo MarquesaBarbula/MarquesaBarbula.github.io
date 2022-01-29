db98451aaf68c87465410cdbae9f48d03efa4c32
Mariana Barnola <marianabarnola@gmail.com>
2022-01-29 13:47:19 +0100

    without countUp code deleted from html and JS

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// SCRIPT TIME COUNTER
-
-// const timeContainer = document.getElementById("time-container");
-// const BIRTH_DAY = "02-23-2021";
-// const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
-// const intlNumberFormatter = new Intl.NumberFormat("en-US");
-
-// setInterval(() => {
-//  const now = new Date();
-//  const difference = Math.floor(
-//    (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
-//  );
-
-//  timeContainer.innerText = intlNumberFormatter.format(difference);
-// }, 1000);
 
 
 // SCRIPT DARK MODE
